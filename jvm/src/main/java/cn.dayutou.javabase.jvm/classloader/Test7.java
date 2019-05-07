52a42cc3ee81b3f3c123d5b1e45f268ffe7417a1
大芋头 <dddyutou@163.com>
2019-05-07 21:20:32 +0800

    修正-并测试git

--- jvm/src/main/java/cn.dayutou.javabase.jvm/classloader/Test7.java
+++ jvm/src/main/java/cn.dayutou.javabase.jvm/classloader/Test7.java
@@ -8,7 +8,7 @@ import sun.misc.Launcher;
  */
 public class Test7 {
     public static void main(String[] args) {
-        System.out.printlConnectn(ClassLoader.class.getClassLoader());
+        System.out.println(ClassLoader.class.getClassLoader());
         System.out.println(Launcher.class.getClassLoader());
     }
 }
